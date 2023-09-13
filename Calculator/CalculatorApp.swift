d3fe8b6d1ef3208e859fb2925518fd04db27da2d
Azizbek Ismailov <3@azizbek.ru>
2023-09-13 13:54:36 +0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Azizbek on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
